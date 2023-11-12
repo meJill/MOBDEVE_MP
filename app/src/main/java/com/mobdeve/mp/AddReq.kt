7651ec9dbc2e218f85f7a970c5624fbf13568e08
meJill <jemill_ferrer@dlsu.edu.ph>
2023-11-12 14:23:37 +0800

    + Button working

---
+++ app/src/main/java/com/mobdeve/mp/AddReq.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.mp
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.mp.R
+
+class AddReq : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_req)
+    }
+}
--- app/src/main/java/com/mobdeve/mp/MainActivity.kt
+++ app/src/main/java/com/mobdeve/mp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.mobdeve.mp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -9,7 +11,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var data: ArrayList<PostModel>
-
     private lateinit var recyclerView: RecyclerView
     private lateinit var myAdapter: MyAdapter
 
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize the data, recyclerView and adapter
+        // Initialize the data, recyclerView, and adapter
         this.data = DataHelper.initializeData()
         this.recyclerView = findViewById(R.id.horizontalRv)
         this.myAdapter = MyAdapter(this.data)
@@ -26,16 +27,16 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPreferences = getSharedPreferences("com.mobdeve.yourname.exercise3lifecyclesp", MODE_PRIVATE)
 
-
         // Initialize the view type and hide like button settings
-
-
+        // Add your code for initializing view type and hiding like button
 
         // Sets the adapter of the recycler view
         this.recyclerView.adapter = this.myAdapter
 
-
-
-
+        val buttonClick = findViewById<Button>(R.id.add_button)
+        buttonClick?.setOnClickListener {
+            val intent = Intent(this, AddReq::class.java)
+            startActivity(intent)
+        }
     }
 }
