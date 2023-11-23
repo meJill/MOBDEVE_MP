8c9b2620caf7955e32da646da17baf5233209a0b
meJill <jemill_ferrer@dlsu.edu.ph>
2023-11-23 19:15:04 +0800

    Reset Design to match what I sent in disc, now aiming to recreate and edit a similar application

---
+++ app/src/main/java/com/mobdeve/mp/fragments/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.mobdeve.mp.fragments;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.mobdeve.mp.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
