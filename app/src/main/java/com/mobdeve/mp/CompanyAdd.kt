e40702d7b5772c2b6d15ceba1874114f1f754251
AkimFA <104549356+AkimFA@users.noreply.github.com>
2023-11-24 20:23:40 +0800

    Company Login Name Change
    After doing company login, name should display above

--- app/src/main/java/com/mobdeve/mp/CompanyAdd.kt
+++ app/src/main/java/com/mobdeve/mp/CompanyAdd.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 private lateinit var dashBoardButton: Button
@@ -13,6 +14,9 @@ class CompanyAdd: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in_company)
+
+        val name = intent.getStringExtra("Company_Name")
+        findViewById<TextView>(R.id.companyName_Edit).text = name
 
         dashBoardButton = findViewById(R.id.company_Dashboard)
 
--- app/src/main/java/com/mobdeve/mp/CompanyLogin.kt
+++ app/src/main/java/com/mobdeve/mp/CompanyLogin.kt
@@ -38,6 +38,7 @@ class CompanyLogin : AppCompatActivity(){
             if (dbHelper.isCompanyUsernamePasswordMatch(findViewById<EditText>(R.id.companyL_name).text.toString(), findViewById<EditText>(R.id.companyL_password).text.toString())) {
                 println("it worked")
                 val intent = Intent(this, CompanyAdd:: class.java)
+                intent.putExtra("Company_Name", findViewById<EditText>(R.id.companyL_name).text.toString())
                 startActivity(intent)
             } else
                 println("no it didnt")
